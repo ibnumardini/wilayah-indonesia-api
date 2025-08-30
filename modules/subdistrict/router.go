75d5ec45cb9c106452269d27d856b933f4a7e2c6
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2025-08-30 19:56:09 +0700

    fix: update route path for FindByQuery to use /maps/{searchQuery}

--- modules/subdistrict/router.go
+++ modules/subdistrict/router.go
@@ -5,7 +5,7 @@ import "github.com/go-chi/chi"
 func newRouter(h *handler) *chi.Mux {
 	r := chi.NewMux()
 
-	r.Get("/search/{searchQuery}", h.FindByQuery)
+	r.Get("/maps/{searchQuery}", h.FindByQuery)
 	r.Get("/{districtCode}", h.FindByDistrictCode)
 
 	return r
